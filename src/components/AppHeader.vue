cb504061dd2466cbf5ace042b067227ed3c4288e
benjaminkent <benjamin.k.jehl@gmail.com>
2019-12-08 17:39:14 -0500

    added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>Vue 3 Scoreboard</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AppHeader',
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding: 2rem 1rem;
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    margin: 0;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h3>home page</h3>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+export default createComponent({
+  name: 'Home',
+  setup() {},
+})
 </script>
